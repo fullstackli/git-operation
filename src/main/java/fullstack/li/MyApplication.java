cd3f6d6ed44aa84ecd165f343b34e0d29ebef3fa
Lijiajun <1307640502@qq.com>
2025-02-10 18:31:39 +0800

    init project with springboot-starter:3.4.2 + jdk21

---
+++ src/main/java/fullstack/li/MyApplication.java
@@ -0,0 +1,17 @@
+package fullstack.li;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Lijiajun
+ * @Date 2025/2/10 - 18:06
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
